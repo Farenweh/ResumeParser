996d932d53d657b138343b41687ae672a12bf52e
Farenweh <57565535+Farenweh@users.noreply.github.com>
2023-07-14 18:19:06 +0800

    Update 纵列排版形态分割演示picSegerDev.py

--- 纵列排版形态分割演示picSegerDev.py
+++ 纵列排版形态分割演示picSegerDev.py
@@ -2,13 +2,18 @@ import cv2
 import numpy as np
 
 
-def find_min_change_column(img):
+def find_min_change_column(img, demo=False):
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
     # 将图像转为灰度图
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # 将灰度图转为二值图
     ret, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     # 获取二值图的宽度和高度
     height, width = binary.shape[:2]
+    # 膨胀
+    binary = cv2.bitwise_not(binary)
+    kernel = np.ones((6, 6), np.uint8)
+    binary = cv2.dilate(binary, kernel, iterations=2)
     # 获取中间80%的宽度
     start = int(width * 0.1)
     end = int(width * 0.9)
@@ -32,21 +37,20 @@ def find_min_change_column(img):
                 min_distance = distance
     min_change_column_index += start
     # 在图像上标记变化次数最少的那一列像素
-    img[:, min_change_column_index-5:min_change_column_index] = [0, 0, 255]
+    img[:, min_change_column_index - 3:min_change_column_index + 3] = [0, 0, 255]
+    binary = cv2.resize(binary, (0, 0), fx=0.25, fy=0.25)
+    cv2.imshow('形态学膨胀', binary)
+    img = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+    cv2.imshow('纵列分割', img)
+    cv2.waitKey(0)
 
-    return min_change_column_index, change_count[min_change_column_index-start]
+    return min_change_column_index, change_count[min_change_column_index - start]
 
 
 if __name__ == '__main__':
     # 加载图像
     img = cv2.imread('109.jpg')
     # 找到变化次数最少的那一列像素，返回其列号和变化次数，并将其用红色竖线标记在图像上
-    column_index, change_count = find_min_change_column(img)
-    print('变化次数最少的那一列像素所在的列号为：', column_index)
-    print('变化次数最少的那一列像素的变化次数为：', change_count)
-
-    # 显示图像
-    img = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    find_min_change_column(img, demo=True)
+    # print('变化次数最少的那一列像素所在的列号为：', column_index)
+    # print('变化次数最少的那一列像素的变化次数为：', change_count)
